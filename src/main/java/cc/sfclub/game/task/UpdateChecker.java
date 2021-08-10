36cf63a3f472760371432a4d512315eae7e78477
iceBear67 <icebear67@sfclub.cc>
2021-08-10 23:04:11 +0800

    Add UpdateChecker Update zh_CN locale file

---
+++ src/main/java/cc/sfclub/game/task/UpdateChecker.java
@@ -0,0 +1,61 @@
+/*
+ *
+ *     Oyster - The universal minigame framework for spigot servers.
+ *     Copyright (C) 2021 SaltedFish Club
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *     USA
+ */
+
+package cc.sfclub.game.task;
+
+import cc.sfclub.game.util.Log;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import lombok.SneakyThrows;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class UpdateChecker extends BukkitRunnable {
+    private static final JsonParser jsonParser = new JsonParser();
+    private final HttpClient httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(6)).build();
+    private final String updateCheckUrl = "https://api.github.com/repos/saltedfishclub/Oyster/releases";
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        Log.transInfo("oyster.update.checking", "");
+        var request = HttpRequest.newBuilder().GET().uri(new URI(updateCheckUrl)).build();
+        var body = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        var ja = jsonParser.parse(body == null ? "[]" : body);
+        for (JsonElement jsonElement : ja.getAsJsonArray()) {
+            var jo = jsonElement.getAsJsonObject();
+            var branch = jo.get("target_commitish").toString();
+            var name = jo.get("name").toString();
+            var tag = jo.get("tag_name").toString();
+            var isDraftOrPreRelease = jo.get("draft").getAsBoolean() || jo.get("prerelease").getAsBoolean();
+            if (isDraftOrPreRelease || !"release".equals(branch)) {
+                return;
+            }
+            Log.transInfo("oyster.update.hint", tag, name);
+            return;
+        }
+    }
+}
