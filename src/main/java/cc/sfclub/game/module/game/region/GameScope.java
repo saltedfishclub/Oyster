7b5bb6708c435415816eac39a3d03e32b68bdcd9
iceBear67 <icebear67@sfclub.cc>
2021-08-10 13:48:24 +0800

    [WIP] GameScope

--- src/main/java/cc/sfclub/game/module/game/region/GameScope.java
+++ src/main/java/cc/sfclub/game/module/game/region/GameScope.java
@@ -19,14 +19,7 @@
  *     USA
  */
 
-package cc.sfclub.game.mechanic.team;
-import org.bukkit.event.Listener;
-import org.jetbrains.annotations.ApiStatus;
+package cc.sfclub.game.module.game.region;
 
-/**
- * It is recommended that you register FlagType as a listener, which helps you implements the functionality of it.
- */
-@ApiStatus.AvailableSince("0.0.1")
-public abstract class FlagType implements Listener {
-    public abstract String getName();
+public class GameScope {
 }
